aba2718929381b3e3c407faeaa13c882701a56e5
jangheon1234 <jheon1024@gmail.com>
2025-04-07 15:49:06 +0900

    k

--- app/front/movie_js.js
+++ app/front/movie_js.js
@@ -11,9 +11,12 @@ let MovieObject = {
             console.log(response)
             movielist = response.result
 
-            topdiv = document.createElement("div")
-            topdiv.style = "column-count: 5"
-            document.body.appendChild(topdiv)
+            // topdiv = document.createElement("div")
+            // topdiv.style = "column-count: 5"
+            // document.body.appendChild(topdiv)
+
+            topdiv = document.getElementById("alldiv")
+
 
             movielist.forEach(movie => {
                 cmovie = document.createElement("div")
@@ -22,7 +25,7 @@ let MovieObject = {
                 mimg = document.createElement("img")
                 mimg.className = "card-img-top"
                 mimg.src = movie.poster_path
-                // mimg.style.cursor = "pointer";
+                mimg.style.cursor = "pointer";
 
                 mimg.onclick = function(){
                     window.open(movie.url);
@@ -38,7 +41,56 @@ let MovieObject = {
         // 실패코드
             console.log(response)
         });
-    }
+    },
+        getgenres : function(){
+            const genresel = document.getElementById("sgenre");
+            const select = genresel.value;
+            //genre = document.getElementById("sgenre").value.toLowerCase()
+            $.ajax({
+            // 실행할 코드
+                type:"GET",
+                // url:`http://localhost:8000/genresbest/${genre}`
+                url: `http://localhost:8000/genresbest/${select}`
+
+            }).done(function(response){
+            // 성공코드
+                console.log(response)
+                movielist = response.result
+
+                // topdiv = document.createElement("div")
+                // topdiv.style = "column-count: 5"
+                // document.body.appendChild(topdiv)
+
+                topdiv = document.getElementById("genrediv")
+                // topdiv.innerHTML = ""; // 기존영화내용을 없애는 첫번쨰 방법
+                while(topdiv.firstChild){                           // 두번쨰 방법
+                    topdiv.removeChild(topdiv.firstChild);
+                }
+
+                movielist.forEach(movie => {
+                    cmovie = document.createElement("div")
+                    cmovie.className = "card"
+
+                    mimg = document.createElement("img")
+                    mimg.className = "card-img-top"
+                    mimg.src = movie.poster_path
+                    mimg.style.cursor = "pointer";
+
+                    mimg.onclick = function(){
+                        window.open(movie.url);
+                    }
+                    // mimg.onmouseover = function(){
+                    //     mimg.style.cursor = "pointer"
+                    // }
+                    cmovie.appendChild(mimg)
+                    topdiv.appendChild(cmovie)
+                })
+
+            }).fail(function(response){
+            // 실패코드
+                console.log(response)
+            });
+        }
 }
 
 MovieObject.init();
